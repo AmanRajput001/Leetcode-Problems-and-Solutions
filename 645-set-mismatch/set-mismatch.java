11c68816ebdb2e2760311516d767f4c14ffe6a76
Aman Rajput <87659146+AmanRajput001@users.noreply.github.com>
2024-02-04 22:34:00 +0530

    Time: 3 ms (71.14%) | Memory: 45.9 MB (25.18%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] arr) {
+
+// cyclic sort
+        for(int i = 0; i < arr.length;){
+            int correct = arr[i] - 1;
+            if(arr[correct] == arr[i]){
+                i++;
+            }else{
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            }
+        }
+
+        //find duplicate
+        for(int i = 0; i < arr.length;){
+            if(i == arr[i] - 1){
+                i++;
+            }else{
+                return new int[]{arr[i], i+1};
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+}
