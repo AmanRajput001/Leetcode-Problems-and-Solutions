4dcba66159d676056c1d135fde7d0ec5509ba7d1
Aman Rajput <87659146+AmanRajput001@users.noreply.github.com>
2024-02-08 21:51:40 +0530

    Time: 1 ms (55.29%) | Memory: 40.7 MB (50.03%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,19 +1,30 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n == 1) {
+        if(n <= 0){
+            return false;
+        }
+        else if (n == 1) {
             return true;
         } else {
-            if(n % 2 != 0){
-                return false; // odd number
-            }
+            /*
+            // Approach 1:
 
-            for (int i = 2; i <= n && i != 0; i *= 2) {
-                if (i == n) {
-                    return true;
+                if(n % 2 != 0){
+                    return false; // odd number
                 }
-            }
+    
+                for (int i = 2; i <= n && i != 0; i *= 2) {
+                    if (i == n) {
+                        return true;
+                    }
+                }
+    
+                return false;
+            */
 
-            return false;
+            // Approach 2:
+
+            return n % 2 == 0 && isPowerOfTwo(n/2);
         }
     }
 }
