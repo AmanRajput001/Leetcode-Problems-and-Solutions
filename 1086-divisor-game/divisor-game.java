0ce56740d009baa330de0c3741c8f7180ae3fad6
Aman Rajput <87659146+AmanRajput001@users.noreply.github.com>
2024-02-19 22:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (55.68%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        boolean alice = false, bob = true;
+
+
+        for( int i = n - 1; i != 0; i--){
+
+            if(bob == false){
+                alice = false;
+                bob = true;
+            }else{
+                alice = true;
+                bob = false;
+            }
+
+            
+            //System.out.println(alice + " " + bob + " " + n);
+            n = n - 1;
+
+
+        }
+
+        return alice;
+    }
+}
