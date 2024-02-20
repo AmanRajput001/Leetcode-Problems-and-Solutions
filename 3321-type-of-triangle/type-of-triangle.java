6dc9d589b427ff7479672782e94cb669fcdcca53
Aman Rajput <87659146+AmanRajput001@users.noreply.github.com>
2024-02-20 23:32:56 +0530

    Time: 1 ms (61.28%) | Memory: 42 MB (56.24%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String triangleType(int[] ar) {
+
+        int a = Math.abs(ar[0] + ar[1]);
+        int b = Math.abs(ar[1] + ar[2]);
+        int c = Math.abs(ar[2] + ar[0]);
+
+        if (a == b && b == c && c == a) {
+            return "equilateral";
+        } else if ((ar[0] == ar[1] && (ar[1] != ar[2]) && (a > ar[2])) || 
+          (ar[1] == ar[2] && (ar[1] != ar[0]) && (b > ar[0])) ||
+          ((ar[2] != ar[1]) && ar[2] == ar[0]) && (c > ar[1])) {
+            return "isosceles";
+        } else if ((a > ar[2] && b > ar[0] && c > ar[1])) {
+            // System.out.println(a + " " + ar[2] + " " +  b + " " + ar[0] + " " + c + " " + ar[1]);
+            return "scalene";
+        }
+        return "none";
+    }
+}
